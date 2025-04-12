ea9dfe6b561b25fa4d45feeda618be5de04b939b
stefanirajagukguk <stefanirajagukguk@gmail.com>
2025-04-12 22:38:27 +0700

    tampillan
    Membuat halaman untuk jadwal kelas dan rekapitulasi untukk guru

--- lib/pages/guru/jadwal_kehadiran_guru.dart
+++ lib/pages/guru/jadwal_kehadiran_guru.dart
@@ -13,76 +13,217 @@ class JadwalKehadiranGuru extends StatefulWidget {
 }
 
 class _JadwalKehadiranGuruState extends State<JadwalKehadiranGuru> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 2;
 
   void _onItemTapped(int index) {
     if (index == 0) {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => HomepageGuru()));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const HomepageGuru()));
     } else if (index == 1) {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => RekapitulasiGuru()));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const RekapitulasiGuru()));
     } else if (index == 2) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => JadwalKehadiranGuru()));
+      // Tetap di halaman ini
     } else if (index == 3) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => RiwayatKehadiranGuru()));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const RiwayatKehadiranGuru()));
     } else if (index == 4) {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => ProfilGuru()));
-    } else {
-      setState(() {
-        _selectedIndex = index;
-      });
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const ProfilGuru()));
     }
+
+    setState(() {
+      _selectedIndex = index;
+    });
   }
+
+  final List<Map<String, dynamic>> jadwalMingguan = [
+    {
+      "hari": "Senin",
+      "jadwal": [
+        {"jam": "08:20 - 10:00", "kelas": "7-1"},
+        {"jam": "10:20 - 12:00", "kelas": "8-1"},
+        {"jam": "13:20 - 14.00", "kelas": "9-1"},
+        {"jam": "15:20 - 16.00", "kelas": "9-2"},
+      ]
+    },
+    {
+      "hari": "Selasa",
+      "jadwal": [
+        {"jam": "08:20 - 10:00", "kelas": "7-1"},
+        {"jam": "10:20 - 12:00", "kelas": "8-1"},
+        {"jam": "13:20 - 14.00", "kelas": "9-1"},
+        {"jam": "15:20 - 16.00", "kelas": "9-2"},
+      ]
+    },
+    {
+      "hari": "Rabu",
+      "jadwal": [
+        {"jam": "08:20 - 10:00", "kelas": "7-1"},
+        {"jam": "10:20 - 12:00", "kelas": "8-1"},
+      ]
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange,
-      body: Stack(
+      backgroundColor: const Color(0xFFF2F2F2),
+      body: Column(
         children: [
           Container(
-            height: 280,
+            padding: const EdgeInsets.only(top: 50, left: 16, right: 16, bottom: 24),
             decoration: const BoxDecoration(
-                color: Color(0xFFEADFC9),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40))),
+              color: Color(0xFFEADFC9),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: const Icon(Icons.arrow_back, color: Colors.black),
+                ),
+                const SizedBox(height: 16),
+                const Center(
+                  child: Text(
+                    'Jadwal',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF1B3C2F),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const Center(
+                  child: Text(
+                    'Semester 2\nT.A. 2023/2024',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFF1B3C2F),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 12),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              itemCount: jadwalMingguan.length,
+              itemBuilder: (context, index) {
+                final hari = jadwalMingguan[index]["hari"];
+                final jadwal = List<Map<String, String>>.from(jadwalMingguan[index]["jadwal"]);
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.05),
+                        blurRadius: 4,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          hari,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF1B3C2F),
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        ...jadwal.map(
+                          (item) => Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 6.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  item["jam"] ?? "",
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                                Container(
+                                  constraints: const BoxConstraints(minWidth: 70),
+                                  padding: const EdgeInsets.symmetric(vertical: 8),
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                  color: Color(0xFF5DAD6E),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Text(
+                                  item["kelas"] ?? "",
+                                  style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                               ),
+                              ),
+                            ),
+
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
       bottomNavigationBar: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(24), topRight: Radius.circular(24)),
-            child: BottomNavigationBar(
-              backgroundColor: const Color(0xFF1B3C2F),
-              type: BottomNavigationBarType.fixed,
-              selectedItemColor: Colors.green,
-              unselectedItemColor: Colors.white,
-              elevation: 0,
-              currentIndex: _selectedIndex,
-              onTap: _onItemTapped,
-              items: const [
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.home, size: 20), label: 'Beranda'),
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.checkSquare, size: 20),
-                    label: 'Rekapitulasi'),
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.list, size: 20), label: 'Jadwal'),
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.history, size: 20),
-                    label: 'RIwayat Kehadiran'),
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.user, size: 20), label: 'Profil'),
-              ],
-            ),
-          )),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(24),
+            topRight: Radius.circular(24),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: const Color(0xFF1B3C2F),
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.green,
+            unselectedItemColor: Colors.white,
+            elevation: 0,
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(LucideIcons.home, size: 20),
+                label: 'Beranda',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(LucideIcons.checkSquare, size: 20),
+                label: 'Rekapitulasi',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(LucideIcons.list, size: 20),
+                label: 'Jadwal',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(LucideIcons.history, size: 20),
+                label: 'Riwayat Kehadiran',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(LucideIcons.user, size: 20),
+                label: 'Profil',
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/pages/guru/rekapitulasi_guru.dart
+++ lib/pages/guru/rekapitulasi_guru.dart
@@ -1,6 +1,6 @@
-import 'package:aplikasi_hadirin/pages/guru/riwayat_kehadiran_guru.dart';
 import 'package:flutter/material.dart';
 import 'package:lucide_icons/lucide_icons.dart';
+import 'package:aplikasi_hadirin/pages/guru/riwayat_kehadiran_guru.dart';
 import 'package:aplikasi_hadirin/pages/guru/profil_guru.dart';
 import 'package:aplikasi_hadirin/pages/guru/jadwal_kehadiran_guru.dart';
 import 'package:aplikasi_hadirin/pages/guru/homepage_guru.dart';
@@ -13,7 +13,7 @@ class RekapitulasiGuru extends StatefulWidget {
 }
 
 class _RekapitulasiGuruState extends State<RekapitulasiGuru> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 1;
 
   void _onItemTapped(int index) {
     if (index == 0) {
@@ -41,48 +41,191 @@ class _RekapitulasiGuruState extends State<RekapitulasiGuru> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: Stack(
+      backgroundColor: const Color(0xFFF5F5F5), // Ubah warna background utama
+      body: Column(
         children: [
           Container(
-            height: 280,
             decoration: const BoxDecoration(
-                color: Color(0xFFEADFC9),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40))),
+              color: Color(0xFFEADFC9),
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(30),
+              ),
+            ),
+            padding: const EdgeInsets.only(
+              top: 40,
+              left: 16,
+              right: 16,
+              bottom: 16,
+            ),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back, color: Color(0xFF205040)),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                const Expanded(
+                  child: Center(
+                    child: Text(
+                      "Lampiran Rekapitulasi",
+                      style: TextStyle(
+                        color: Color(0xFF205040),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 48),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 16),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListView(
+                children: [
+                  const SizedBox(height: 16),
+                  DataRekapitulasiWidget(
+                    kelas: 'Kelas 7 - 1',
+                    tahunAjaran: 'Tahun ajaran 2022/2023',
+                    imageUrl: 'assets/images/rekap.png',
+                  ),
+                  const SizedBox(height: 16),
+                  DataRekapitulasiWidget(
+                    kelas: 'Kelas 7 - 2',
+                    tahunAjaran: 'Tahun ajaran 2022/2023',
+                    imageUrl: 'assets/images/rekap.png',
+                  ),
+                  const SizedBox(height: 16),
+                  DataRekapitulasiWidget(
+                    kelas: 'Kelas 7 - 3',
+                    tahunAjaran: 'Tahun ajaran 2022/2023',
+                    imageUrl: 'assets/images/rekap.png',
+                  ),
+                  const SizedBox(height: 16),
+                  DataRekapitulasiWidget(
+                    kelas: 'Kelas 8 - 1',
+                    tahunAjaran: 'Tahun ajaran 2022/2023',
+                    imageUrl: 'assets/images/rekap.png',
+                  ),
+                  const SizedBox(height: 16),
+                  DataRekapitulasiWidget(
+                    kelas: 'Kelas 8 - 2',
+                    tahunAjaran: 'Tahun ajaran 2022/2023',
+                    imageUrl: 'assets/images/rekap.png',
+                  ),
+                  const SizedBox(height: 16),
+                  DataRekapitulasiWidget(
+                    kelas: 'Kelas 9 - 1',
+                    tahunAjaran: 'Tahun ajaran 2022/2023',
+                    imageUrl: 'assets/images/rekap.png',
+                  ),
+                  const SizedBox(height: 16),
+                  DataRekapitulasiWidget(
+                    kelas: 'Kelas 9 - 2',
+                    tahunAjaran: 'Tahun ajaran 2022/2023',
+                    imageUrl: 'assets/images/rekap.png',
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
+            ),
           ),
         ],
       ),
+
       bottomNavigationBar: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(24), topRight: Radius.circular(24)),
-            child: BottomNavigationBar(
-              backgroundColor: const Color(0xFF1B3C2F),
-              type: BottomNavigationBarType.fixed,
-              selectedItemColor: Colors.green,
-              unselectedItemColor: Colors.white,
-              elevation: 0,
-              currentIndex: _selectedIndex,
-              onTap: _onItemTapped,
-              items: const [
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.home, size: 20), label: 'Beranda'),
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.checkSquare, size: 20),
-                    label: 'Rekapitulasi'),
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.list, size: 20), label: 'Jadwal'),
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.history, size: 20),
-                    label: 'RIwayat Kehadiran'),
-                BottomNavigationBarItem(
-                    icon: Icon(LucideIcons.user, size: 20), label: 'Profil'),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(24), topRight: Radius.circular(24)),
+          child: BottomNavigationBar(
+            backgroundColor: const Color(0xFF1B3C2F),
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: const Color(0xFF94BCAC),
+            unselectedItemColor: Colors.white,
+            elevation: 0,
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(LucideIcons.home, size: 20), label: 'Beranda'),
+              BottomNavigationBarItem(
+                  icon: Icon(LucideIcons.checkSquare, size: 20),
+                  label: 'Rekapitulasi'),
+              BottomNavigationBarItem(
+                  icon: Icon(LucideIcons.list, size: 20), label: 'Jadwal'),
+              BottomNavigationBarItem(
+                  icon: Icon(LucideIcons.history, size: 20),
+                  label: 'Riwayat Kehadiran'),
+              BottomNavigationBarItem(
+                  icon: Icon(LucideIcons.user, size: 20), label: 'Profil'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DataRekapitulasiWidget extends StatelessWidget {
+  final String kelas;
+  final String tahunAjaran;
+  final String imageUrl;
+
+  DataRekapitulasiWidget({
+    required this.kelas,
+    required this.tahunAjaran,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF94BCAC),
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            imageUrl,
+            width: 50,
+            height: 50,
+          ),
+          const SizedBox(width: 16.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  kelas,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  tahunAjaran,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
               ],
             ),
-          )),
+          ),
+        ],
+      ),
     );
   }
 }
